9ebdff64c3c283c3ca83ba024aebab2f13416b82
Elroy <elroy1820@gmail.com>
2023-05-11 13:22:20 +0800

    LOGO

--- src/layout/sidebar/component/Brand.tsx
+++ src/layout/sidebar/component/Brand.tsx
@@ -1,5 +1,5 @@
 // Chakra imports
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
 import { HorizonLogo } from "~/components/icons/Icons";
 import { HSeparator } from "~/components/separator";
 
@@ -9,7 +9,9 @@ const SidebarBrand = () => {
 
   return (
     <Flex alignItems="center" flexDirection="column">
-      <HorizonLogo h="26px" w="175px" my="32px" color={logoColor} />
+      <Text h="26px" w="175px" my="32px" color={logoColor} fontSize={20}>
+        Theme Admin
+      </Text>
       <HSeparator mb="20px" />
     </Flex>
   );
